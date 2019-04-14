e63e92ea0abb9bc3d811131d2c865c3c284343a8
Eliemrson FOnseca Rego <eliemerson.rego@programmers.com.br>
2019-04-14 20:31:46 -0300

    HomeWork Jilian Done

---
+++ ReceptorText/ReceptorMensage/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ReceptorMensage
+{
+    class MainClass
+    {
+
+        static void Main()
+        {
+
+
+            MainClass main = new MainClass();
+            TcpListener server = null;
+            TcpClient client = null;
+            NetworkStream stream = null;
+
+            Byte[] bytes = new Byte[701264];
+            String data = null;
+            try
+            {
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddr, port);
+                server.Start();
+
+
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+                    client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+                    data = null;
+                    stream = client.GetStream();
+                    int i;
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    main.Inserir(data);
+                    client.Close();
+
+
+                }
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine("Can't connect with the server, try again", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+
+        }
+
+
+        public void Inserir(string ceps)
+        {
+           
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = "sendregex.database.windows.net,1433";
+            builder.UserID = "AdminRegex";
+            builder.Password = "Eliemerson@123";
+            builder.InitialCatalog = "RegezDataBase";
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                Console.WriteLine("\nQuery data example:");
+                Console.WriteLine("=========================================\n");
+
+               
+             string sql = "INSERT INTO TABELA (id, ceps) VALUES (@id, @ceps)";
+                
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                   var id = Guid.NewGuid();
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@ceps", ceps);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+
+            }
+
+        }
+    }
+}
+
+
---
+++ SendText/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SendFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkStream stream = null;
+            TcpListener listener = null;
+            BinaryWriter writer = null;
+            BinaryReader reader = null;
+
+          
+            try
+            {
+                Int32 port = 13000;
+                TcpClient client = new TcpClient("127.0.0.1", port);
+
+                Console.WriteLine("Cliente ------> Trying to connect, wait...");
+                Console.WriteLine("Reading the doc, wait...");
+
+                string[] text = System.IO.File.ReadAllLines(@"C:\dev\git\HomeWorkJilian\mockData.txt");
+                string formatPattern = @"[0-9]{5}-[\d]{3}";
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    var filter = Regex.Match(text[i], formatPattern);
+                    builder.Append(filter).Append("");                    
+                }
+
+                
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\dev\git\HomeWorkJilian\WriteData.txt");
+                file.WriteLine(builder.ToString());
+                file.Close();
+                string RegexText = System.IO.File.ReadAllText(@"C:\dev\git\HomeWorkJilian\WriteData.txt");
+
+
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(RegexText);
+                stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0}", data);
+
+                var sizebyte = data.Length;
+
+                Console.WriteLine("Size bye {0} in data send", sizebyte);
+
+                data = new Byte[sizebyte];
+                String responseData = String.Empty;
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                stream.Close();
+                client.Close();
+                Console.ReadKey();
+
+            }
+            catch (WebException e) { }
+        }
+    }
+
+}
